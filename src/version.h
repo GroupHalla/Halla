b6dc2b054c66a5a499de88543e1069f50c8be051
farleybarbosa320-oss <farleybarbosa320-oss@users.noreply.github.com>
2026-08-02 23:06:52 +0000

    Bump client version to 3.13.15 for final update release

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 
 namespace halla {
 constexpr const char* kAppName    = "Halla";
-constexpr const char* kAppVersion = "3.13.14";
+constexpr const char* kAppVersion = "3.13.15";
 constexpr const char* kAppBuild   = "estável";
 constexpr const char* kOrgName    = "Halla";
 }
