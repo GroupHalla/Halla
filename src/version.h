0d5cda48590b0471cda124b6d224408062c03890
Halla Development <halla-development@users.noreply.github.com>
2026-08-05 18:30:09 +0000

    fix: corrigir catálogo de traduções

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 
 namespace halla {
 constexpr const char* kAppName    = "Halla";
-constexpr const char* kAppVersion = "3.13.56";
+constexpr const char* kAppVersion = "3.13.57";
 constexpr const char* kAppBuild   = "estável";
 constexpr const char* kOrgName    = "Halla";
 }
