6643364163c8c96040d309d481da0cfa5368dc5d
Halla Development <halla-development@users.noreply.github.com>
2026-08-06 01:17:20 +0000

    release: manter ultima atualizacao como v1.0.0 OFICIAL

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 
 namespace halla {
 constexpr const char* kAppName    = "Halla";
-constexpr const char* kAppVersion = "1.0.0";
+constexpr const char* kAppVersion = "3.13.62";
 constexpr const char* kAppBuild   = "estável";
 constexpr const char* kOrgName    = "Halla";
 }
